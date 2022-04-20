38a61a89de904fd6fee541715dc9073bf75341d8
kareem96 <ak339444@gmail.com>
2022-04-20 16:15:52 +0700

    update 20/04/2022

--- settings.gradle
+++ settings.gradle
@@ -1,10 +1,10 @@
-pluginManagement {
+/*pluginManagement {
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
     }
-}
+}*/
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
